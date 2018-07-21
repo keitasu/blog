77f2a20affaa293e69470b739336939c298e4c9b
suke <suke083.tech@gmail.com>
2018-07-22 01:00:32 +0900

    PWA対応

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-transformer-remark',
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
